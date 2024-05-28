cc25f9953b48d7ffb033c46cf958ea7f7deb5164
Rimas74 <gintilas.rimantas@gmail.com>
2024-05-28 22:01:35 +0300

    Add project files.

---
+++ PersonRegistrationSystem.DataAccess/Entities/Person.cs
@@ -0,0 +1,55 @@
+﻿using PersonRegistrationSystem.DataAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRegistrationSystem.DataAccess.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name can only contain letters and spaces.")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last Name must be between 2 and 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name can only contain letters and spaces.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        [EnumDataType(typeof(Gender), ErrorMessage = "Gender must be either 'Male' or 'Female'.")]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Personal Identification Code must be 11 digits.")]
+        public string PersonalCode { get; set; }
+
+        [Required]
+        [Phone]
+        public string TelephoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string ProfilePhotoPath { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public PlaceOfResidence PlaceOfResidence { get; set; }
+    }
+}
---
+++ PersonRegistrationSystem.DataAccess/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRegistrationSystem.DataAccess.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Username must be between 8 and 20 characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+
+        [Required]
+        public string Role { get; set; } // 'Admin' or 'User'
+
+        public ICollection<Person> Persons { get; set; }
+    }
+}
